7e3ab55b39c5aff0161b736c244f4620933adfa0
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-05-19 22:50:37 +0900

    submit:pairpro03

---
+++ form.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  $('.text').on('click', function(){
+      $('.container h1').css('color', 'gray');
+      $('.contact').css('background-color', 'pink');
+      $('.submit').css('background-color', 'gray');
+      $('p').css('color', 'white');
+  });
+});
